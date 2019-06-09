b1d2b5fa2d08e56212f312aff73eca143e2fa92d
dahnbalan <dahnbalan@dal.ca>
2019-06-09 20:20:01 -0300

    first commit

---
+++ api/models/Actor.js
@@ -0,0 +1,52 @@
+/**
+ * Actor.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    firstname: {
+      type: 'string',
+      required: true,
+      maxLength: 200,
+      example: 'Leonardo'
+    },
+
+    lastname: {
+      type: 'string',
+      required: true,
+      maxLength: 200,
+      example: 'DiCaprio'
+    },
+
+    age: {
+      type: 'number',
+      required: true,
+      example: 55
+    },
+
+    gender: {
+      type: 'string',
+      required: true,
+      example: 'male'
+    }
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
---
+++ api/models/Movie.js
@@ -0,0 +1,60 @@
+/**
+ * Movie.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    title: {
+      type: 'string',
+      required: true,
+      maxLength: 200,
+      example: 'Inception'
+    },
+
+    year: {
+      type: 'number',
+      required: true,
+      example: 2010
+    },
+
+    runtime: {
+      type: 'number',
+      required: true,
+      example: 148
+    },
+
+    language: {
+      type: 'string',
+      required: true,
+      maxLength: 200,
+      example: 'English'
+    },
+
+    release_country: {
+      type: 'string',
+      required: true,
+      maxLength: 200,
+      example: 'USA'
+    }
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
