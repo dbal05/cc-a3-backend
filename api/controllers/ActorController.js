893be7df4093eca6d7ee56ca89202ca8f497b4a5
dahnbalan <dahnbalan@dal.ca>
2019-06-10 20:20:31 -0300

    Updated controller, added new methods

--- api/controllers/ActorController.js
+++ api/controllers/ActorController.js
@@ -6,58 +6,80 @@
  */
 
 module.exports = {
-    // getActors889: async function(req, res) {
-    //     // get params from req
-    //     // query Actor with params from req
-    //     // return results
-    //     var actors = await Actor.find();
-    //     return res.json({actors: actors});
-    // },
+    getActors889: async function(req, res) {
+        // get params from req
+        // query Actor with params from req
+        // return results
+        var actors = await Actor.find().populate('movies');
+        return res.json({actors: actors});
+    },
 
-    // getActorById889: async function(req, res) {
-    //     var id = req.query.id;
-    //     var actor = await Actor.find({
-    //         id: id
-    //     });
+    getActorById889: async function(req, res) {
+        var id = req.query.id;
+        var actor = await Actor.find({
+            id: id
+        }).populate('movies');
 
-    //     return res.json({actor: actor});
-    // },
+        return res.json({actor: actor});
+    },
 
-    // getActorByName889: async function(req, res) {
-    //     var firstname = req.query.firstname;
-    //     var lastname = req.query.lastname;
-    //     var actor = await Actor.find({
-    //         firstname: firstname,
-    //         lastname: lastname 
-    //     });
+    getActorByName889: async function(req, res) {
+        var firstname = req.query.firstname;
+        var lastname = req.query.lastname;
+        var actor = await Actor.find({
+            firstname: firstname,
+            lastname: lastname 
+        }).populate('movies');
 
-    //     return res.json({
-    //         actor: actor
-    //     });
-    // },
+        return res.json({
+            actor: actor
+        });
+    },
 
-    // addMovieForActorById889: async function(req, res) {
-    //     var actorId = req.body.actorId;
-    //     var movieId = req.body.movieId;
+    getActorByAge889: async function(req, res) {
+        var minAge = req.query.minAge;
+        var maxAge = req.query.maxAge;
+        if (minAge && maxAge) {
+            var actor = await Actor.find({
+                age: {'<=': maxAge, '>=': minAge} 
+            }).populate('movies'); 
+        } else if (minAge && !maxAge) {
+            var actor = await Actor.find({
+                age: {'>=': minAge} 
+            }).populate('movies'); 
+        } else if (!minAge && maxAge) {
+            var actor = await Actor.find({
+                age: {'<=': maxAge} 
+            }).populate('movies');
+        }
+        
+        return res.json({
+            actor: actor
+        });
+    },
 
-    //     await Actor.addToCollection(actorId, 'movies', movieId);
-    //     var actor = await Actor.find({
-    //         id: id
-    //     });
+    addMovieForActorById889: async function(req, res) {
+        var actorId = req.body.actorId;
+        var movieId = req.body.movieId;
 
-    //     return res.json({
-    //         actor: actor
-    //     });
-    // }, 
+        await Actor.addToCollection(actorId, 'movies', movieId);
+        var actor = await Actor.find({
+            id: id
+        }).populate('movies');
 
-    // deleteActorById889: async function(req, res) {
-    //     var id = req.query.id;
-    //     var destroyedActor = await Actor.destroy({
-    //         id: id
-    //     });
+        return res.json({
+            actor: actor
+        });
+    }, 
 
-    //     return res.json({actor: destroyedActor});
-    // }, 
+    deleteActorById889: async function(req, res) {
+        var id = req.query.id;
+        var destroyedActor = await Actor.destroy({
+            id: id
+        });
+
+        return res.json({actor: destroyedActor});
+    }, 
 
 };
 
--- config/routes.js
+++ config/routes.js
@@ -20,20 +20,22 @@ module.exports.routes = {
   ***************************************************************************/
 
   '/': { view: 'pages/homepage' },
-  // 'GET /actor/getActors889': 'ActorController.getActors889',
-  // 'GET /actor/getActorById889': 'ActorController.getActorById889',
-  // 'GET /actor/getActorByName889': 'ActorController.getActorByName889',
-  // 'POST /actor/addMovieForActorById889': 'ActorController.addMovieForActorById889',
-  // 'DELETE /actor/deleteActorById889': 'ActorController.deleteActorById889',
+  'GET /actor/getActors889': 'ActorController.getActors889',
+  'GET /actor/getActorById889': 'ActorController.getActorById889',
+  'GET /actor/getActorByName889': 'ActorController.getActorByName889',
+  'GET /actor/getActorByAge889': 'ActorController.getActorByAge889',
+  
+  'POST /actor/addMovieForActorById889': 'ActorController.addMovieForActorById889',
+  'DELETE /actor/deleteActorById889': 'ActorController.deleteActorById889',
 
-  // 'GET /movie/getMovies889': 'MovieController.getMovies889',
-  // 'GET /movie/getMovieById889': 'MovieController.getMovieById889',
-  // 'GET /movie/getMovieByTitle889': 'MovieController.getMovieByTitle889',
-  // 'GET /movie/getMoviesByYear889': 'MovieController.getMoviesByYear889',
-  // 'GET /movie/getMoviesByRuntime889': 'MovieController.getMoviesByRuntime889',
-  // 'GET /movie/getMoviesByCountry889': 'MovieController.getMoviesByCountry889',
-  // 'POST /movie/addActorForMovieById889': 'MovieController.addActorForMovieById889',
-  // 'DELETE /movie/deleteMovieById889': 'MovieController.deleteMovieById889',
+  'GET /movie/getMovies889': 'MovieController.getMovies889',
+  'GET /movie/getMovieById889': 'MovieController.getMovieById889',
+  'GET /movie/getMovieByTitle889': 'MovieController.getMovieByTitle889',
+  'GET /movie/getMoviesByYear889': 'MovieController.getMoviesByYear889',
+  'GET /movie/getMoviesByRuntime889': 'MovieController.getMoviesByRuntime889',
+  'GET /movie/getMoviesByCountry889': 'MovieController.getMoviesByCountry889',
+  'POST /movie/addActorForMovieById889': 'MovieController.addActorForMovieById889',
+  'DELETE /movie/deleteMovieById889': 'MovieController.deleteMovieById889',
 
   /***************************************************************************
   *                                                                          *
