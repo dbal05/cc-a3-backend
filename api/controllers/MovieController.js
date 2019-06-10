7713abbbb902d55b13784e872f56c92d06b06e15
dahnbalan <dahnbalan@dal.ca>
2019-06-10 00:58:14 -0300

    Just updated all the methods and now commented out the methods because Blueprint (built-in orm) can basically do everything

--- api/controllers/MovieController.js
+++ api/controllers/MovieController.js
@@ -6,88 +6,88 @@
  */
 
 module.exports = {
-    getMovies889: async function(req, res) {
-        // get params from req
-        // query Movie with params from req
-        // return results
-        var Movies = await Movie.find();
-        return res.json({movies: movies});
-    },
+    // getMovies889: async function(req, res) {
+    //     // get params from req
+    //     // query Movie with params from req
+    //     // return results
+    //     var Movies = await Movie.find();
+    //     return res.json({movies: movies});
+    // },
 
-    getMovieById889: async function(req, res) {
-        var id = req.query.id;
-        var movie = await Movie.find({
-            id: id
-        });
+    // getMovieById889: async function(req, res) {
+    //     var id = req.query.id;
+    //     var movie = await Movie.find({
+    //         id: id
+    //     });
 
-        return res.json({movie: movie});
-    },
+    //     return res.json({movie: movie});
+    // },
 
-    getMovieByTitle889: async function(req, res) {
-        var title = req.query.title;
-        var movie = await Movie.find({
-            title: title
-        });
+    // getMovieByTitle889: async function(req, res) {
+    //     var title = req.query.title;
+    //     var movie = await Movie.find({
+    //         title: title
+    //     });
 
-        return res.json({
-            movie: movie
-        });
-    },
+    //     return res.json({
+    //         movie: movie
+    //     });
+    // },
 
-    getMoviesByYear889: async function(req, res) {
-        var year = req.query.year;
-        var movies = await Movie.find({
-            year: year
-        });
+    // getMoviesByYear889: async function(req, res) {
+    //     var year = req.query.year;
+    //     var movies = await Movie.find({
+    //         year: year
+    //     });
 
-        return res.json({
-            movies: movies
-        });
-    },
+    //     return res.json({
+    //         movies: movies
+    //     });
+    // },
 
-    getMoviesByRuntime889: async function(req, res) {
-        var runtime = req.query.runtime;
-        var movies = await Movie.find({
-            runtime: runtime
-        });
+    // getMoviesByRuntime889: async function(req, res) {
+    //     var runtime = req.query.runtime;
+    //     var movies = await Movie.find({
+    //         runtime: runtime
+    //     });
 
-        return res.json({
-            movies: movies
-        });
-    },
+    //     return res.json({
+    //         movies: movies
+    //     });
+    // },
 
-    getMoviesByCountry889: async function(req, res) {
-        var country = req.query.country;
-        var movies = await Movie.find({
-            release_country: country
-        });
+    // getMoviesByCountry889: async function(req, res) {
+    //     var country = req.query.country;
+    //     var movies = await Movie.find({
+    //         release_country: country
+    //     });
 
-        return res.json({
-            movies: movies
-        });
-    },
+    //     return res.json({
+    //         movies: movies
+    //     });
+    // },
 
-    addActorForMovieById889: async function(req, res) {
-        var actorId = req.body.actorId;
-        var movieId = req.body.movieId;
+    // addActorForMovieById889: async function(req, res) {
+    //     var actorId = req.body.actorId;
+    //     var movieId = req.body.movieId;
 
-        await Movie.addToCollection(movieId, 'actors', actorId);
-        var movie = await Movie.find({
-            id: movieId
-        });
+    //     await Movie.addToCollection(movieId, 'actors', actorId);
+    //     var movie = await Movie.find({
+    //         id: movieId
+    //     });
 
-        return res.json({
-            movie: movie
-        });
-    },
+    //     return res.json({
+    //         movie: movie
+    //     });
+    // },
 
-    deleteMovieById889: async function(req, res) {
-        var id = req.query.id;
-        var destroyedMovie = await Movie.destroy({
-            id: id
-        });
+    // deleteMovieById889: async function(req, res) {
+    //     var id = req.query.id;
+    //     var destroyedMovie = await Movie.destroy({
+    //         id: id
+    //     });
 
-        return res.json({movie: destroyedMovie});
-    }, 
+    //     return res.json({movie: destroyedMovie});
+    // }, 
 };
 
